03774846b24658277ba663611f9c7c08327f9b10
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-01-05 01:23:17 +0100

    express.js added

--- nodeJS/NODE-FIRST-APP/app.js
+++ nodeJS/NODE-FIRST-APP/app.js
@@ -1,8 +1,23 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer((routes) => {
+app.use("/", (req, res, next) => {
+    console.log("This always runs");
+    next();
 });
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+    console.log("In another middleware");
+    res.send("<h1>The 'Add Product' Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+    console.log("In another middleware");
+    res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
