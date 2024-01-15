bc0ef1b2a09e270e5812dabc5d3c539b46aa49bf
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-01-15 10:47:31 +0100

    dynamic content and templates
    * pug package added
    
    * handlebars package added
    
    * ejs package added
    
    * cleanup
    
    * new assignment done

--- nodeJS/NODE-FIRST-APP/app.js
+++ nodeJS/NODE-FIRST-APP/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -18,10 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("404", {pageTitle: "Page Not Found"});
+    res.status(404).render("404", {pageTitle: "Page Not Found", path: "Error"});
 });
 
 app.listen(3000);
-
-// const server = http.createServer(app);
-// server.listen(3000);
